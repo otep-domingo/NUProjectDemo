bbb960d2c44e268ac63d3b00fbeab08762688bea
Prof Prof <domingo.joseph@outlook.ph>
2023-12-07 15:46:22 +0800

    Add project files.

---
+++ NUProjectDemo/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NUProjectDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            int status = 0; //0 for maintenance, 1 for ok status
+            ViewData["Message"] = "This handles all student concerns in NU. You must have an account to access the other modules.";
+            if (status == 0)
+            {
+                return View("Views/ErrorCollection/Maintenance.cshtml");
+            }
+            else
+            {
+                return View("Views/Home/Privacy.cshtml");
+            }
+            return View("Views/Home/Privacy.cshtml");
+        }
+    }
+    
+}
---
+++ NUProjectDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NUProjectDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
